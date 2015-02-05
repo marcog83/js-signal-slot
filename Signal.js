df70c385d457698fca8eeb5358fe9243aff18c33
marcog83 <marcogobbi0@gmail.com>
2015-02-05 23:58:29 +0100

    bug-fix

--- Signal.js
+++ Signal.js
@@ -1,6 +1,11 @@
 !function(root, factory){
     if (typeof define === 'function' && define.amd) {
         define([], factory);
+    } else if (typeof exports === 'object') {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like enviroments that support module.exports,
+        // like Node.
+        module.exports = factory();
     } else {
         root.Signal = factory();
     }
